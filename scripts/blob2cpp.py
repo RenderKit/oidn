a74d3bf0343c408b92134dafbe6103b2ffc75e9d
Attila Afra <attila.t.afra@intel.com>
2018-08-04 18:15:50 +0300

    minor python fix

--- scripts/blob2cpp.py
+++ scripts/blob2cpp.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 
 ## ======================================================================== ##
 ## Copyright 2009-2018 Intel Corporation                                    ##
