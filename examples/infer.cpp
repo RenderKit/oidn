624fe4bc433a866bf660799fecf181b86bc66bc0
Attila Afra <attila.t.afra@intel.com>
2018-11-26 11:42:40 +0200

    examples cosmetics

--- examples/infer.cpp
+++ examples/infer.cpp
@@ -55,9 +55,9 @@ int main(int argc, char **argv)
     oidn::FilterRef filter = device.newFilter("Autoencoder");
 
     const size_t F = sizeof(float);
-    filter.setImage("color", input.data, oidn::Format::Float3, W, H, 0 * F, 9 * F);
-    filter.setImage("albedo", input.data, oidn::Format::Float3, W, H, 3 * F, 9 * F);
-    filter.setImage("normal", input.data, oidn::Format::Float3, W, H, 6 * F, 9 * F);
+    filter.setImage("color",  input.data,  oidn::Format::Float3, W, H, 0 * F, 9 * F);
+    filter.setImage("albedo", input.data,  oidn::Format::Float3, W, H, 3 * F, 9 * F);
+    filter.setImage("normal", input.data,  oidn::Format::Float3, W, H, 6 * F, 9 * F);
     filter.setImage("output", output.data, oidn::Format::Float3, W, H, 0 * F, 3 * F);
     filter.set1i("srgb", 1);
 
