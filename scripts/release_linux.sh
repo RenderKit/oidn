1d58a21d54e6ea7638eda0d94eac3cebc821e255
Attila Afra <attila.t.afra@intel.com>
2019-01-19 16:36:09 +0200

    minor Linux release script update

--- scripts/release_linux.sh
+++ scripts/release_linux.sh
@@ -62,7 +62,7 @@ TBB_DIR=$DEP_DIR/$TBB_BUILD
 if [ ! -d $TBB_DIR ]; then
   TBB_URL=https://github.com/01org/tbb/releases/download/$TBB_VERSION/${TBB_BUILD}_lin.tgz
   wget $TBB_URL
-  tar xaf `basename $TBB_URL`
+  tar -xf `basename $TBB_URL`
 fi
 
 # Get the number of build threads
