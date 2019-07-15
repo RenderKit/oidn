71c4f17be0092325450324c1d5735a25769d7b55
Attila Afra <attila.t.afra@intel.com>
2019-07-16 02:17:40 +0300

    upgraded to TBB 2019 Update 8

--- scripts/release_windows.bat
+++ scripts/release_windows.bat
@@ -38,8 +38,8 @@ set DEP_DIR=%ROOT_DIR%\deps
 cd %DEP_DIR%
 
 rem Set up TBB
-set TBB_VERSION=2019_U5
-set TBB_BUILD=tbb2019_20190320oss
+set TBB_VERSION=2019_U8
+set TBB_BUILD=tbb2019_20190605oss
 set TBB_DIR=%DEP_DIR%\%TBB_BUILD%
 if not exist %TBB_DIR% (
   echo Error: %TBB_DIR% is missing
