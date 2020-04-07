3c899f43006a8008867798ed8634a1caecb59d5f
Attila Afra <attila.t.afra@intel.com>
2020-04-07 20:30:21 +0300

    denoise: adjusted error threshold

--- examples/denoise.cpp
+++ examples/denoise.cpp
@@ -299,7 +299,7 @@ int main(int argc, char* argv[])
         else
           re = std::abs(expect - actual);
         if (maxre < re) maxre = re;
-        if (re > 1e-4)
+        if (re > 1e-3)
         {
           //std::cout << "i=" << i << " expect=" << expect << " actual=" << actual << std::endl;
           ++nerr;
