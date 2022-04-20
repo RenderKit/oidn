8c5c41adef2a71c88dd0ba45adb08578ef1a58ea
Attila Afra <attila.t.afra@intel.com>
2022-04-20 18:15:18 +0300

    fixed cuDNN and TBB build issues on Windows

--- cmake/FindcuDNN.cmake
+++ cmake/FindcuDNN.cmake
@@ -17,29 +17,21 @@
 
 find_package(CUDA 10.0 REQUIRED)
 
-set(cudnn_inc_dir_hints "${CMAKE_CUDA_TOOLKIT_INCLUDE_DIRECTORIES};/usr/local/cuda/include")
-find_path(CUDNN_INCLUDE_DIR "cudnn.h" HINTS ${cudnn_inc_dir_hints})
+set(CUDNN_ROOT ${CUDA_TOOLKIT_ROOT_DIR} CACHE PATH "Folder containing NVIDIA cuDNN")
 
-find_library(CUDNN_LIBRARY cudnn)
-find_library(CUDA_DRIVER_LIBRARY cuda)
-# this is work around to avoid duplication half creation in both cuda and SYCL
-
-find_package(Threads REQUIRED)
+find_path(CUDNN_INCLUDE_DIR "cudnn.h" HINTS ${CUDNN_ROOT} PATH_SUFFIXES include)
 
 include(FindPackageHandleStandardArgs)
 
 find_library(
     CUDNN_LIBRARY cudnn
-    HINTS ${CUDA_TOOLKIT_ROOT_DIR}
+    HINTS ${CUDNN_ROOT}
     PATH_SUFFIXES lib lib64 bin)
 
 find_package_handle_standard_args(cuDNN
     REQUIRED_VARS
         CUDNN_INCLUDE_DIR
-        CUDA_INCLUDE_DIRS
         CUDNN_LIBRARY
-        CUDA_LIBRARIES
-        CUDA_DRIVER_LIBRARY
 )
 
 # extract version from the include
@@ -69,12 +61,7 @@ endif()
 if(NOT TARGET cuDNN::cuDNN)
   add_library(cuDNN::cuDNN SHARED IMPORTED)
   set_target_properties(cuDNN::cuDNN PROPERTIES
-      IMPORTED_LOCATION
-      ${CUDNN_LIBRARY}
-      INTERFACE_INCLUDE_DIRECTORIES
-      "${CUDA_INCLUDE_DIRS};${CUDNN_INCLUDE_DIR}"
-      INTERFACE_LINK_LIBRARIES
-      "Threads::Threads;${CUDA_DRIVER_LIBRARY};${CUDA_LIBRARIES}"
-      INTERFACE_COMPILE_DEFINITIONS
-      CUDA_NO_HALF)
+      IMPORTED_LOCATION ${CUDNN_LIBRARY}
+      IMPORTED_IMPLIB ${CUDNN_LIBRARY}
+      INTERFACE_INCLUDE_DIRECTORIES ${CUDNN_INCLUDE_DIR})
 endif()
