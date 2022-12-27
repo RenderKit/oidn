8afe40b872e8e023ab54c4d87126ba7b8f226aba
Attila Afra <attila.t.afra@intel.com>
2022-12-27 18:12:57 +0200

    moved external projects to 'external' directory

--- cmake/oidn_dnnl.cmake
+++ cmake/oidn_dnnl.cmake
@@ -1,6 +1,8 @@
 ## Copyright 2009-2022 Intel Corporation
 ## SPDX-License-Identifier: Apache-2.0
 
+set(DNNL_ROOT "external/mkl-dnn")
+
 set(DNNL_VERSION_MAJOR 2)
 set(DNNL_VERSION_MINOR 2)
 set(DNNL_VERSION_PATCH 4)
@@ -25,75 +27,75 @@ option(DNNL_ENABLE_ITT_TASKS
 mark_as_advanced(DNNL_ENABLE_ITT_TASKS)
 
 configure_file(
-  "${PROJECT_SOURCE_DIR}/mkl-dnn/include/oneapi/dnnl/dnnl_config.h.in"
-  "${PROJECT_BINARY_DIR}/mkl-dnn/include/oneapi/dnnl/dnnl_config.h"
+  "${PROJECT_SOURCE_DIR}/${DNNL_ROOT}/include/oneapi/dnnl/dnnl_config.h.in"
+  "${PROJECT_BINARY_DIR}/${DNNL_ROOT}/include/oneapi/dnnl/dnnl_config.h"
 )
 configure_file(
-  "${PROJECT_SOURCE_DIR}/mkl-dnn/include/oneapi/dnnl/dnnl_version.h.in"
-  "${PROJECT_BINARY_DIR}/mkl-dnn/include/oneapi/dnnl/dnnl_version.h"
+  "${PROJECT_SOURCE_DIR}/${DNNL_ROOT}/include/oneapi/dnnl/dnnl_version.h.in"
+  "${PROJECT_BINARY_DIR}/${DNNL_ROOT}/include/oneapi/dnnl/dnnl_version.h"
 )
 
 file(GLOB DNNL_SOURCES
-  mkl-dnn/src/common/*.[ch]
-  mkl-dnn/src/common/*.[ch]pp
-  mkl-dnn/src/cpu/bfloat16.cpp
-  mkl-dnn/src/cpu/binary_injector_utils.[ch]pp
-  mkl-dnn/src/cpu/cpu_concat.cpp
-  mkl-dnn/src/cpu/cpu_concat_pd.hpp
-  mkl-dnn/src/cpu/cpu_convolution_list.cpp
-  mkl-dnn/src/cpu/cpu_convolution_pd.hpp
-  mkl-dnn/src/cpu/cpu_engine.[ch]pp
-  mkl-dnn/src/cpu/cpu_memory_storage.hpp
-  mkl-dnn/src/cpu/cpu_pooling_list.cpp
-  mkl-dnn/src/cpu/cpu_pooling_pd.hpp
-  mkl-dnn/src/cpu/cpu_primitive.hpp
-  mkl-dnn/src/cpu/cpu_stream.hpp
-  mkl-dnn/src/cpu/cpu_sum.cpp
-  mkl-dnn/src/cpu/platform.[ch]pp
-  mkl-dnn/src/cpu/simple_q10n.hpp
-  mkl-dnn/src/cpu/jit_utils/*.[ch]pp
-  mkl-dnn/src/cpu/reorder/cpu_reorder.[ch]pp
-  mkl-dnn/src/cpu/reorder/cpu_reorder_regular_f32_f32.cpp
-  mkl-dnn/src/cpu/reorder/simple_reorder.hpp
-  mkl-dnn/src/cpu/x64/cpu_barrier.[ch]pp
-  mkl-dnn/src/cpu/x64/cpu_isa_traits.[ch]pp
-  mkl-dnn/src/cpu/x64/cpu_reducer.[ch]pp
-  mkl-dnn/src/cpu/x64/jit_avx2_conv_kernel_f32.[ch]pp
-  mkl-dnn/src/cpu/x64/jit_avx2_convolution.[ch]pp
-  mkl-dnn/src/cpu/x64/jit_avx512_common_convolution.[ch]pp
-  mkl-dnn/src/cpu/x64/jit_avx512_common_conv_kernel.[ch]pp
-  mkl-dnn/src/cpu/x64/jit_avx512_core_bf16cvt.[ch]pp
-  mkl-dnn/src/cpu/x64/jit_generator.hpp
-  mkl-dnn/src/cpu/x64/jit_primitive_conf.hpp
-  mkl-dnn/src/cpu/x64/jit_sse41_conv_kernel_f32.[ch]pp
-  mkl-dnn/src/cpu/x64/jit_sse41_convolution.[ch]pp
-  mkl-dnn/src/cpu/x64/jit_transpose_src_utils.[ch]pp
-  mkl-dnn/src/cpu/x64/jit_uni_eltwise.[ch]pp
-  mkl-dnn/src/cpu/x64/jit_uni_pooling.[ch]pp
-  mkl-dnn/src/cpu/x64/jit_uni_pool_kernel.[ch]pp
-  mkl-dnn/src/cpu/x64/jit_uni_reorder.[ch]pp
-  mkl-dnn/src/cpu/x64/jit_uni_reorder_utils.cpp
-  mkl-dnn/src/cpu/x64/injectors/*.[ch]pp
-  mkl-dnn/src/cpu/x64/xbyak/*.h
+  ${DNNL_ROOT}/src/common/*.[ch]
+  ${DNNL_ROOT}/src/common/*.[ch]pp
+  ${DNNL_ROOT}/src/cpu/bfloat16.cpp
+  ${DNNL_ROOT}/src/cpu/binary_injector_utils.[ch]pp
+  ${DNNL_ROOT}/src/cpu/cpu_concat.cpp
+  ${DNNL_ROOT}/src/cpu/cpu_concat_pd.hpp
+  ${DNNL_ROOT}/src/cpu/cpu_convolution_list.cpp
+  ${DNNL_ROOT}/src/cpu/cpu_convolution_pd.hpp
+  ${DNNL_ROOT}/src/cpu/cpu_engine.[ch]pp
+  ${DNNL_ROOT}/src/cpu/cpu_memory_storage.hpp
+  ${DNNL_ROOT}/src/cpu/cpu_pooling_list.cpp
+  ${DNNL_ROOT}/src/cpu/cpu_pooling_pd.hpp
+  ${DNNL_ROOT}/src/cpu/cpu_primitive.hpp
+  ${DNNL_ROOT}/src/cpu/cpu_stream.hpp
+  ${DNNL_ROOT}/src/cpu/cpu_sum.cpp
+  ${DNNL_ROOT}/src/cpu/platform.[ch]pp
+  ${DNNL_ROOT}/src/cpu/simple_q10n.hpp
+  ${DNNL_ROOT}/src/cpu/jit_utils/*.[ch]pp
+  ${DNNL_ROOT}/src/cpu/reorder/cpu_reorder.[ch]pp
+  ${DNNL_ROOT}/src/cpu/reorder/cpu_reorder_regular_f32_f32.cpp
+  ${DNNL_ROOT}/src/cpu/reorder/simple_reorder.hpp
+  ${DNNL_ROOT}/src/cpu/x64/cpu_barrier.[ch]pp
+  ${DNNL_ROOT}/src/cpu/x64/cpu_isa_traits.[ch]pp
+  ${DNNL_ROOT}/src/cpu/x64/cpu_reducer.[ch]pp
+  ${DNNL_ROOT}/src/cpu/x64/jit_avx2_conv_kernel_f32.[ch]pp
+  ${DNNL_ROOT}/src/cpu/x64/jit_avx2_convolution.[ch]pp
+  ${DNNL_ROOT}/src/cpu/x64/jit_avx512_common_convolution.[ch]pp
+  ${DNNL_ROOT}/src/cpu/x64/jit_avx512_common_conv_kernel.[ch]pp
+  ${DNNL_ROOT}/src/cpu/x64/jit_avx512_core_bf16cvt.[ch]pp
+  ${DNNL_ROOT}/src/cpu/x64/jit_generator.hpp
+  ${DNNL_ROOT}/src/cpu/x64/jit_primitive_conf.hpp
+  ${DNNL_ROOT}/src/cpu/x64/jit_sse41_conv_kernel_f32.[ch]pp
+  ${DNNL_ROOT}/src/cpu/x64/jit_sse41_convolution.[ch]pp
+  ${DNNL_ROOT}/src/cpu/x64/jit_transpose_src_utils.[ch]pp
+  ${DNNL_ROOT}/src/cpu/x64/jit_uni_eltwise.[ch]pp
+  ${DNNL_ROOT}/src/cpu/x64/jit_uni_pooling.[ch]pp
+  ${DNNL_ROOT}/src/cpu/x64/jit_uni_pool_kernel.[ch]pp
+  ${DNNL_ROOT}/src/cpu/x64/jit_uni_reorder.[ch]pp
+  ${DNNL_ROOT}/src/cpu/x64/jit_uni_reorder_utils.cpp
+  ${DNNL_ROOT}/src/cpu/x64/injectors/*.[ch]pp
+  ${DNNL_ROOT}/src/cpu/x64/xbyak/*.h
 )
 
 if(DNNL_ENABLE_JIT_PROFILING OR DNNL_ENABLE_ITT_TASKS)
-  file(GLOB ITT_SOURCES mkl-dnn/src/common/ittnotify/*.[ch])
+  file(GLOB ITT_SOURCES ${DNNL_ROOT}/src/common/ittnotify/*.[ch])
   list(APPEND DNNL_SOURCES ${ITT_SOURCES})
 endif()
 if(DNNL_ENABLE_JIT_PROFILING)
   list(APPEND DNNL_SOURCES
-    mkl-dnn/src/cpu/jit_utils/linux_perf/linux_perf.cpp
-    mkl-dnn/src/cpu/jit_utils/linux_perf/linux_perf.hpp
+    ${DNNL_ROOT}/src/cpu/jit_utils/linux_perf/linux_perf.cpp
+    ${DNNL_ROOT}/src/cpu/jit_utils/linux_perf/linux_perf.hpp
   )
 endif()
 
 if(CMAKE_CXX_COMPILER_ID STREQUAL "MSVC")
   file(GLOB DNNL_SOURCES_BIGOBJ
-    mkl-dnn/src/common/memory_zero_pad.cpp
-    mkl-dnn/src/cpu/cpu_engine.cpp
-    mkl-dnn/src/cpu/reorder/cpu_reorder_regular_f32_f32.cpp
-    mkl-dnn/src/cpu/cpu_convolution_list.cpp
+    ${DNNL_ROOT}/src/common/memory_zero_pad.cpp
+    ${DNNL_ROOT}/src/cpu/cpu_engine.cpp
+    ${DNNL_ROOT}/src/cpu/reorder/cpu_reorder_regular_f32_f32.cpp
+    ${DNNL_ROOT}/src/cpu/cpu_convolution_list.cpp
   )
   set_source_files_properties(${DNNL_SOURCES_BIGOBJ} PROPERTIES COMPILE_FLAGS "/bigobj")
 endif()
@@ -102,12 +104,12 @@ add_library(dnnl STATIC ${DNNL_SOURCES})
 
 target_include_directories(dnnl
   PUBLIC
-    $<BUILD_INTERFACE:${PROJECT_SOURCE_DIR}/mkl-dnn/include>
-    $<BUILD_INTERFACE:${PROJECT_BINARY_DIR}/mkl-dnn/include>
-    $<BUILD_INTERFACE:${PROJECT_SOURCE_DIR}/mkl-dnn/src>
-    $<BUILD_INTERFACE:${PROJECT_SOURCE_DIR}/mkl-dnn/src/common>
-    $<BUILD_INTERFACE:${PROJECT_SOURCE_DIR}/mkl-dnn/src/cpu>
-    $<BUILD_INTERFACE:${PROJECT_SOURCE_DIR}/mkl-dnn/src/cpu/xbyak>
+    $<BUILD_INTERFACE:${PROJECT_SOURCE_DIR}/${DNNL_ROOT}/include>
+    $<BUILD_INTERFACE:${PROJECT_BINARY_DIR}/${DNNL_ROOT}/include>
+    $<BUILD_INTERFACE:${PROJECT_SOURCE_DIR}/${DNNL_ROOT}/src>
+    $<BUILD_INTERFACE:${PROJECT_SOURCE_DIR}/${DNNL_ROOT}/src/common>
+    $<BUILD_INTERFACE:${PROJECT_SOURCE_DIR}/${DNNL_ROOT}/src/cpu>
+    $<BUILD_INTERFACE:${PROJECT_SOURCE_DIR}/${DNNL_ROOT}/src/cpu/xbyak>
 )
 
 target_compile_definitions(dnnl
