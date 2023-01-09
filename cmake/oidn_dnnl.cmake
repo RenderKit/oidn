21bb4590a2194fb1ad6bfdbf4da4a3f92ab33f2b
Attila Afra <attila.t.afra@intel.com>
2023-01-10 00:38:26 +0200

    oneDNN CMake cleanup

--- cmake/oidn_dnnl.cmake
+++ cmake/oidn_dnnl.cmake
@@ -1,7 +1,8 @@
 ## Copyright 2009-2022 Intel Corporation
 ## SPDX-License-Identifier: Apache-2.0
 
-set(DNNL_ROOT "external/mkl-dnn")
+set(DNNL_SOURCE_DIR "${PROJECT_SOURCE_DIR}/external/mkl-dnn")
+set(DNNL_BINARY_DIR "${PROJECT_BINARY_DIR}/external/mkl-dnn")
 
 set(DNNL_VERSION_MAJOR 3)
 set(DNNL_VERSION_MINOR 0)
@@ -31,78 +32,78 @@ set(BUILD_CONVOLUTION TRUE)
 set(BUILD_PRIMITIVE_CPU_ISA_ALL TRUE)
 
 configure_file(
-  "${PROJECT_SOURCE_DIR}/${DNNL_ROOT}/include/oneapi/dnnl/dnnl_config.h.in"
-  "${PROJECT_BINARY_DIR}/${DNNL_ROOT}/include/oneapi/dnnl/dnnl_config.h"
+  "${DNNL_SOURCE_DIR}/include/oneapi/dnnl/dnnl_config.h.in"
+  "${DNNL_BINARY_DIR}/include/oneapi/dnnl/dnnl_config.h"
 )
 configure_file(
-  "${PROJECT_SOURCE_DIR}/${DNNL_ROOT}/include/oneapi/dnnl/dnnl_version.h.in"
-  "${PROJECT_BINARY_DIR}/${DNNL_ROOT}/include/oneapi/dnnl/dnnl_version.h"
+  "${DNNL_SOURCE_DIR}/include/oneapi/dnnl/dnnl_version.h.in"
+  "${DNNL_BINARY_DIR}/include/oneapi/dnnl/dnnl_version.h"
 )
 
 file(GLOB DNNL_SOURCES
-  ${DNNL_ROOT}/src/common/*.[ch]pp
-  ${DNNL_ROOT}/src/cpu/jit_utils/*.[ch]pp
-  ${DNNL_ROOT}/src/cpu/x64/injectors/*.[ch]pp
-  ${DNNL_ROOT}/src/cpu/x64/xbyak/*.h
+  ${DNNL_SOURCE_DIR}/src/common/*.[ch]pp
+  ${DNNL_SOURCE_DIR}/src/cpu/jit_utils/*.[ch]pp
+  ${DNNL_SOURCE_DIR}/src/cpu/x64/injectors/*.[ch]pp
+  ${DNNL_SOURCE_DIR}/src/cpu/x64/xbyak/*.h
 )
 
 list(APPEND DNNL_SOURCES
-  ${DNNL_ROOT}/src/cpu/bfloat16.cpp
-  ${DNNL_ROOT}/src/cpu/binary_injector_utils.cpp
-  ${DNNL_ROOT}/src/cpu/binary_injector_utils.hpp
-  ${DNNL_ROOT}/src/cpu/cpu_concat.cpp
-  ${DNNL_ROOT}/src/cpu/cpu_convolution_list.cpp
-  ${DNNL_ROOT}/src/cpu/cpu_convolution_pd.hpp
-  ${DNNL_ROOT}/src/cpu/cpu_eltwise_pd.hpp
-  ${DNNL_ROOT}/src/cpu/cpu_engine.cpp
-  ${DNNL_ROOT}/src/cpu/cpu_engine.hpp
-  ${DNNL_ROOT}/src/cpu/cpu_memory_storage.hpp
-  ${DNNL_ROOT}/src/cpu/cpu_stream.hpp
-  ${DNNL_ROOT}/src/cpu/cpu_sum.cpp
-  ${DNNL_ROOT}/src/cpu/float16.cpp
-  ${DNNL_ROOT}/src/cpu/platform.cpp
-  ${DNNL_ROOT}/src/cpu/platform.hpp
-  ${DNNL_ROOT}/src/cpu/primitive_attr_postops.cpp
-  ${DNNL_ROOT}/src/cpu/primitive_attr_postops.hpp
-  ${DNNL_ROOT}/src/cpu/scale_utils.cpp
-  ${DNNL_ROOT}/src/cpu/scale_utils.hpp
-  ${DNNL_ROOT}/src/cpu/simple_q10n.hpp
-  ${DNNL_ROOT}/src/cpu/zero_point_utils.cpp
-  ${DNNL_ROOT}/src/cpu/zero_point_utils.hpp
-  ${DNNL_ROOT}/src/cpu/x64/amx_tile_configure.cpp
-  ${DNNL_ROOT}/src/cpu/x64/amx_tile_configure.hpp
-  ${DNNL_ROOT}/src/cpu/x64/cpu_barrier.hpp
-  ${DNNL_ROOT}/src/cpu/x64/cpu_barrier.cpp
-  ${DNNL_ROOT}/src/cpu/x64/cpu_isa_traits.cpp
-  ${DNNL_ROOT}/src/cpu/x64/cpu_isa_traits.hpp
-  ${DNNL_ROOT}/src/cpu/x64/cpu_reducer.cpp
-  ${DNNL_ROOT}/src/cpu/x64/cpu_reducer.hpp
-  ${DNNL_ROOT}/src/cpu/x64/jit_avx2_conv_kernel_f32.cpp
-  ${DNNL_ROOT}/src/cpu/x64/jit_avx2_conv_kernel_f32.hpp
-  ${DNNL_ROOT}/src/cpu/x64/jit_avx2_convolution.cpp
-  ${DNNL_ROOT}/src/cpu/x64/jit_avx2_convolution.hpp
-  ${DNNL_ROOT}/src/cpu/x64/jit_avx512_common_convolution.cpp
-  ${DNNL_ROOT}/src/cpu/x64/jit_avx512_common_convolution.hpp
-  ${DNNL_ROOT}/src/cpu/x64/jit_avx512_common_conv_kernel.cpp
-  ${DNNL_ROOT}/src/cpu/x64/jit_avx512_common_conv_kernel.hpp
-  ${DNNL_ROOT}/src/cpu/x64/jit_generator.hpp
-  ${DNNL_ROOT}/src/cpu/x64/jit_primitive_conf.hpp
-  ${DNNL_ROOT}/src/cpu/x64/jit_sse41_conv_kernel_f32.cpp
-  ${DNNL_ROOT}/src/cpu/x64/jit_sse41_conv_kernel_f32.hpp
-  ${DNNL_ROOT}/src/cpu/x64/jit_sse41_convolution.cpp
-  ${DNNL_ROOT}/src/cpu/x64/jit_sse41_convolution.hpp
-  ${DNNL_ROOT}/src/cpu/x64/jit_transpose_utils.cpp
-  ${DNNL_ROOT}/src/cpu/x64/jit_transpose_utils.hpp
+  ${DNNL_SOURCE_DIR}/src/cpu/bfloat16.cpp
+  ${DNNL_SOURCE_DIR}/src/cpu/binary_injector_utils.cpp
+  ${DNNL_SOURCE_DIR}/src/cpu/binary_injector_utils.hpp
+  ${DNNL_SOURCE_DIR}/src/cpu/cpu_concat.cpp
+  ${DNNL_SOURCE_DIR}/src/cpu/cpu_convolution_list.cpp
+  ${DNNL_SOURCE_DIR}/src/cpu/cpu_convolution_pd.hpp
+  ${DNNL_SOURCE_DIR}/src/cpu/cpu_eltwise_pd.hpp
+  ${DNNL_SOURCE_DIR}/src/cpu/cpu_engine.cpp
+  ${DNNL_SOURCE_DIR}/src/cpu/cpu_engine.hpp
+  ${DNNL_SOURCE_DIR}/src/cpu/cpu_memory_storage.hpp
+  ${DNNL_SOURCE_DIR}/src/cpu/cpu_stream.hpp
+  ${DNNL_SOURCE_DIR}/src/cpu/cpu_sum.cpp
+  ${DNNL_SOURCE_DIR}/src/cpu/float16.cpp
+  ${DNNL_SOURCE_DIR}/src/cpu/platform.cpp
+  ${DNNL_SOURCE_DIR}/src/cpu/platform.hpp
+  ${DNNL_SOURCE_DIR}/src/cpu/primitive_attr_postops.cpp
+  ${DNNL_SOURCE_DIR}/src/cpu/primitive_attr_postops.hpp
+  ${DNNL_SOURCE_DIR}/src/cpu/scale_utils.cpp
+  ${DNNL_SOURCE_DIR}/src/cpu/scale_utils.hpp
+  ${DNNL_SOURCE_DIR}/src/cpu/simple_q10n.hpp
+  ${DNNL_SOURCE_DIR}/src/cpu/zero_point_utils.cpp
+  ${DNNL_SOURCE_DIR}/src/cpu/zero_point_utils.hpp
+  ${DNNL_SOURCE_DIR}/src/cpu/x64/amx_tile_configure.cpp
+  ${DNNL_SOURCE_DIR}/src/cpu/x64/amx_tile_configure.hpp
+  ${DNNL_SOURCE_DIR}/src/cpu/x64/cpu_barrier.hpp
+  ${DNNL_SOURCE_DIR}/src/cpu/x64/cpu_barrier.cpp
+  ${DNNL_SOURCE_DIR}/src/cpu/x64/cpu_isa_traits.cpp
+  ${DNNL_SOURCE_DIR}/src/cpu/x64/cpu_isa_traits.hpp
+  ${DNNL_SOURCE_DIR}/src/cpu/x64/cpu_reducer.cpp
+  ${DNNL_SOURCE_DIR}/src/cpu/x64/cpu_reducer.hpp
+  ${DNNL_SOURCE_DIR}/src/cpu/x64/jit_avx2_conv_kernel_f32.cpp
+  ${DNNL_SOURCE_DIR}/src/cpu/x64/jit_avx2_conv_kernel_f32.hpp
+  ${DNNL_SOURCE_DIR}/src/cpu/x64/jit_avx2_convolution.cpp
+  ${DNNL_SOURCE_DIR}/src/cpu/x64/jit_avx2_convolution.hpp
+  ${DNNL_SOURCE_DIR}/src/cpu/x64/jit_avx512_common_convolution.cpp
+  ${DNNL_SOURCE_DIR}/src/cpu/x64/jit_avx512_common_convolution.hpp
+  ${DNNL_SOURCE_DIR}/src/cpu/x64/jit_avx512_common_conv_kernel.cpp
+  ${DNNL_SOURCE_DIR}/src/cpu/x64/jit_avx512_common_conv_kernel.hpp
+  ${DNNL_SOURCE_DIR}/src/cpu/x64/jit_generator.hpp
+  ${DNNL_SOURCE_DIR}/src/cpu/x64/jit_primitive_conf.hpp
+  ${DNNL_SOURCE_DIR}/src/cpu/x64/jit_sse41_conv_kernel_f32.cpp
+  ${DNNL_SOURCE_DIR}/src/cpu/x64/jit_sse41_conv_kernel_f32.hpp
+  ${DNNL_SOURCE_DIR}/src/cpu/x64/jit_sse41_convolution.cpp
+  ${DNNL_SOURCE_DIR}/src/cpu/x64/jit_sse41_convolution.hpp
+  ${DNNL_SOURCE_DIR}/src/cpu/x64/jit_transpose_utils.cpp
+  ${DNNL_SOURCE_DIR}/src/cpu/x64/jit_transpose_utils.hpp
 )
 
 if(DNNL_ENABLE_JIT_PROFILING OR DNNL_ENABLE_ITT_TASKS)
-  file(GLOB ITT_SOURCES ${DNNL_ROOT}/src/common/ittnotify/*.[ch])
+  file(GLOB ITT_SOURCES ${DNNL_SOURCE_DIR}/src/common/ittnotify/*.[ch])
   list(APPEND DNNL_SOURCES ${ITT_SOURCES})
 endif()
 if(DNNL_ENABLE_JIT_PROFILING)
   list(APPEND DNNL_SOURCES
-    ${DNNL_ROOT}/src/cpu/jit_utils/linux_perf/linux_perf.cpp
-    ${DNNL_ROOT}/src/cpu/jit_utils/linux_perf/linux_perf.hpp
+    ${DNNL_SOURCE_DIR}/src/cpu/jit_utils/linux_perf/linux_perf.cpp
+    ${DNNL_SOURCE_DIR}/src/cpu/jit_utils/linux_perf/linux_perf.hpp
   )
 endif()
 
@@ -116,12 +117,12 @@ add_library(dnnl STATIC ${DNNL_SOURCES})
 
 target_include_directories(dnnl
   PUBLIC
-    $<BUILD_INTERFACE:${PROJECT_SOURCE_DIR}/${DNNL_ROOT}/include>
-    $<BUILD_INTERFACE:${PROJECT_BINARY_DIR}/${DNNL_ROOT}/include>
-    $<BUILD_INTERFACE:${PROJECT_SOURCE_DIR}/${DNNL_ROOT}/src>
-    $<BUILD_INTERFACE:${PROJECT_SOURCE_DIR}/${DNNL_ROOT}/src/common>
-    $<BUILD_INTERFACE:${PROJECT_SOURCE_DIR}/${DNNL_ROOT}/src/cpu>
-    $<BUILD_INTERFACE:${PROJECT_SOURCE_DIR}/${DNNL_ROOT}/src/cpu/xbyak>
+    $<BUILD_INTERFACE:${DNNL_SOURCE_DIR}/include>
+    $<BUILD_INTERFACE:${DNNL_BINARY_DIR}/include>
+    $<BUILD_INTERFACE:${DNNL_SOURCE_DIR}/src>
+    $<BUILD_INTERFACE:${DNNL_SOURCE_DIR}/src/common>
+    $<BUILD_INTERFACE:${DNNL_SOURCE_DIR}/src/cpu>
+    $<BUILD_INTERFACE:${DNNL_SOURCE_DIR}/src/cpu/xbyak>
 )
 
 target_compile_definitions(dnnl
