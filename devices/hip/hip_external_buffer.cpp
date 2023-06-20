661ab8b93d8ae54c6bc9f32014f8934837715b08
xiaoxinguo <guoxx@me.com>
2023-06-20 19:43:18 +0800

    Fix parameters for import CUDA and HIP external memory from shared handle.

--- devices/hip/hip_external_buffer.cpp
+++ devices/hip/hip_external_buffer.cpp
@@ -57,8 +57,11 @@ OIDN_NAMESPACE_BEGIN
       throw Exception(Error::InvalidArgument, "external memory type not supported by the device");
     }
 
-    handleDesc.handle.win32.handle = handle;
-    handleDesc.handle.win32.name = name;
+    if (handle)
+      handleDesc.handle.win32.handle = handle;
+    if (name)
+      handleDesc.handle.win32.name = name;
+
     handleDesc.size = byteSize;
 
     init(handleDesc);
