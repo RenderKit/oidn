e509341ff3bb96128dfacbcef148442cbcafbe1f
Anthony Roberts <anthony.roberts@linaro.org>
2023-09-20 13:08:11 +0100

    Convert non-blocked CPU pooling to ISPC

--- devices/cpu/cpu_pool.cpp
+++ devices/cpu/cpu_pool.cpp
@@ -22,14 +22,13 @@ OIDN_NAMESPACE_BEGIN
     if (!src || !dst)
       throw std::logic_error("pooling source/destination not set");
 
+    ispc::CPUPoolKernel kernel;
+    kernel.src = toISPC<ispc::TensorAccessor3D>(*src);
+    kernel.dst = toISPC<ispc::TensorAccessor3D>(*dst);
+
     if (srcDesc.layout == TensorLayout::Chw8c ||
         srcDesc.layout == TensorLayout::Chw16c)
     {
-
-      ispc::CPUPoolKernel kernel;
-      kernel.src = toISPC<ispc::TensorAccessor3D>(*src);
-      kernel.dst = toISPC<ispc::TensorAccessor3D>(*dst);
-
       // Blocked layout
       const int blockC = getTensorLayoutInfo(dstDesc.layout).blockC;
 
@@ -38,22 +37,12 @@ OIDN_NAMESPACE_BEGIN
         ispc::CPUPoolKernel_run_blocked(&kernel, cb, h);
       });
     }
-    else // CHW
+    else
     {
-      TensorAccessor3D<float, TensorLayout::chw> srcAccessor = *src;
-      TensorAccessor3D<float, TensorLayout::chw> dstAccessor = *dst;
-
+      // CHW layout
       parallel_nd(dst->getPaddedC(), dst->getH(), [&](int c, int h)
       {
-        for(int w = 0; w < dst->getW(); w++)
-        {
-          const float x0 = srcAccessor(c, h*2,   w*2);
-          const float x1 = srcAccessor(c, h*2,   w*2+1);
-          const float x2 = srcAccessor(c, h*2+1, w*2);
-          const float x3 = srcAccessor(c, h*2+1, w*2+1);
-
-          dstAccessor(c, h, w) = math::max(math::max(x0, x1), math::max(x2, x3));
-        }
+        ispc::CPUPoolKernel_run_nonblocked(&kernel, c, h);
       });
     }
   }
