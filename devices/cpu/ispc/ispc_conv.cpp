e5c4e11c7c670ad36c4ecd211663e4e12755639d
Anthony Roberts <anthony.roberts@linaro.org>
2023-09-20 11:08:56 +0100

    Clean up code for C++ implementation

--- devices/cpu/ispc/ispc_conv.cpp
+++ devices/cpu/ispc/ispc_conv.cpp
@@ -23,54 +23,41 @@ OIDN_NAMESPACE_BEGIN
     TensorAccessor3D<float, TensorLayout::chw> srcAccessor = *src;
     TensorAccessor3D<float, TensorLayout::chw> dstAccessor = *dst;
     TensorAccessor1D<float> biasAccessor = *bias;
-    std::stringstream sstream;
-    sstream << "\nSrc CHW is " << src->getPaddedC() << "," << src->getH() << "," << src->getW() << std::endl;
-    sstream << "Dst CHW is " << dst->getPaddedC() << "," << dst->getH() << "," << dst->getW() << std::endl;
-    sstream << "Layout is SRC:" << static_cast<int>(src->getLayout()) << " DST: " << static_cast<int>(dst->getLayout()) << std::endl;
-    sstream << "Weight OIHW is " << weightAccessor.O << "," << weightAccessor.I << "," << weightAccessor.H << "," << weightAccessor.W << std::endl;
-    //std::cout << sstream.str();
 
-    //for(int cb = 0; cb < dst->getPaddedC() / blockC; cb++)
+    // Loop through the output channels (using TBB)
+    //for(int oc = 0; oc < dst->getPaddedC(); oc++)
     // Uncomment line above, and comment out line below (and L85 - bracket and colon) for single-threading
     parallel_nd(dst->getPaddedC(), [&](int oc)
     {
       // Incredibly slow, naive implementation, but here we go - right now, better correct than fast
-      // Loop through the output channels
-      //for(int i = 0; i < blockC; i++)
+      
+      // Loop through output height
+      for(int oh = 0; oh < src->getH(); oh++)
       {
-        //int oc = (cb * blockC) + i;
-        std::stringstream sstream;
-        sstream << "Running for CHW: " << oc << "," << src->getH() << "," << src->getW() << std::endl;
-        //std::cout << sstream.str();
-        
-        // Loop through output height
-        for(int oh = 0; oh < src->getH(); oh++)
+        // Loop through output width
+        for(int ow = 0; ow < src->getW(); ow++)
         {
-          // Loop through output width
-          for(int ow = 0; ow < src->getW(); ow++)
-          {
-            // Initialise the dest value to 0
-            dstAccessor(oc, oh, ow) = 0.0f;
+          // Initialise the dest value to 0
+          dstAccessor(oc, oh, ow) = 0.0f;
 
-            // Loop through the input channels
-            for(int ic = 0; ic < src->getPaddedC(); ic++)
+          // Loop through the input channels
+          for(int ic = 0; ic < src->getPaddedC(); ic++)
+          {
+            // Being in a blocked format means we have to retrieve every value manually
+            for(int ih = (oh == 0 ? 0 : -1); ih <= (oh == (src->getH() - 1) ? 0 : 1); ih++)
             {
-              // Being in a blocked format means we have to retrieve every value manually
-              for(int ih = (oh == 0 ? 0 : -1); ih <= (oh == (src->getH() - 1) ? 0 : 1); ih++)
+              for(int iw = (ow == 0 ? 0 : -1); iw <= (ow == (src->getW() - 1) ? 0 : 1); iw++)
               {
-                for(int iw = (ow == 0 ? 0 : -1); iw <= (ow == (src->getW() - 1) ? 0 : 1); iw++)
-                {
-                  dstAccessor(oc, oh, ow) += (srcAccessor(ic, oh+ih, ow+iw) * ((weightAccessor(oc, ic, ih + 1, iw + 1))));
-                }
+                dstAccessor(oc, oh, ow) += (srcAccessor(ic, oh+ih, ow+iw) * ((weightAccessor(oc, ic, ih + 1, iw + 1))));
               }
             }
-
-            // Add the bias
-            dstAccessor(oc, oh, ow) += biasAccessor(oc);
-
-            // Activator
-            dstAccessor(oc, oh, ow) = max(dstAccessor(oc, oh, ow), 0.0f);
           }
+
+          // Add the bias
+          dstAccessor(oc, oh, ow) += biasAccessor(oc);
+
+          // Activator
+          dstAccessor(oc, oh, ow) = max(dstAccessor(oc, oh, ow), 0.0f);
         }
       }
     }
