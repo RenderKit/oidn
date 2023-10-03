58d57567acd5a0359f16df258b7f60fbea536e24
Attila Afra <attila.t.afra@intel.com>
2023-10-03 17:10:30 +0300

    fixed Python invokation in CMake

--- cmake/oidn_macros.cmake
+++ cmake/oidn_macros.cmake
@@ -48,7 +48,7 @@ function(oidn_generate_cpp_from_blob out_sources namespace)
     add_custom_command(
       OUTPUT ${out_cpp_path} ${out_hpp_path}
       COMMAND ${CMAKE_COMMAND} -E make_directory ${out_dir}
-      COMMAND ${PYTHON_EXECUTABLE}
+      COMMAND ${Python_EXECUTABLE}
       ARGS ${OIDN_ROOT_SOURCE_DIR}/scripts/blob_to_cpp.py ${in_path} -o ${out_cpp_path} -H ${out_hpp_path} -n ${namespace}
       DEPENDS ${in_path}
       COMMENT "Generating CXX source files from blob ${in_path}"
