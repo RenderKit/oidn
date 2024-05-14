ce5f853872c247974449a460f6f446c56cec9dc1
Adellar Irankunda <addyirankunda@gmail.com>
2024-05-13 23:50:00 -0700

    reverting device_factory

--- core/device_factory.h
+++ core/device_factory.h
@@ -9,38 +9,38 @@
 OIDN_NAMESPACE_BEGIN
 
   class DeviceFactory : public RefCount
-{
-public:
-  virtual Ref<Device> newDevice() = 0;
-  virtual Ref<Device> newDevice(const Ref<PhysicalDevice>& physicalDevice) = 0;
-};
+  {
+  public:
+    virtual Ref<Device> newDevice() = 0;
+    virtual Ref<Device> newDevice(const Ref<PhysicalDevice>& physicalDevice) = 0;
+  };
 
-class SYCLDeviceFactoryBase : public DeviceFactory
-{
-public:
-  using DeviceFactory::newDevice;
-  virtual Ref<Device> newDevice(const sycl::queue* queues, int numQueues) = 0;
-};
+  class SYCLDeviceFactoryBase : public DeviceFactory
+  {
+  public:
+    using DeviceFactory::newDevice;
+    virtual Ref<Device> newDevice(const sycl::queue* queues, int numQueues) = 0;
+  };
 
-class CUDADeviceFactoryBase : public DeviceFactory
-{
-public:
-  using DeviceFactory::newDevice;
-  virtual Ref<Device> newDevice(const int* deviceIDs, const cudaStream_t* streams, int numPairs) = 0;
-};
+  class CUDADeviceFactoryBase : public DeviceFactory
+  {
+  public:
+    using DeviceFactory::newDevice;
+    virtual Ref<Device> newDevice(const int* deviceIDs, const cudaStream_t* streams, int numPairs) = 0;
+  };
 
-class HIPDeviceFactoryBase : public DeviceFactory
-{
-public:
-  using DeviceFactory::newDevice;
-  virtual Ref<Device> newDevice(const int* deviceIDs, const hipStream_t* streams, int numPairs) = 0;
-};
+  class HIPDeviceFactoryBase : public DeviceFactory
+  {
+  public:
+    using DeviceFactory::newDevice;
+    virtual Ref<Device> newDevice(const int* deviceIDs, const hipStream_t* streams, int numPairs) = 0;
+  };
 
-class MetalDeviceFactoryBase : public DeviceFactory
-{
-public:
-  using DeviceFactory::newDevice;
-  virtual Ref<Device> newDevice(const MTLCommandQueue_id* commandQueues, int numQueues) = 0;
-};
+  class MetalDeviceFactoryBase : public DeviceFactory
+  {
+  public:
+    using DeviceFactory::newDevice;
+    virtual Ref<Device> newDevice(const MTLCommandQueue_id* commandQueues, int numQueues) = 0;
+  };
 
 OIDN_NAMESPACE_END
